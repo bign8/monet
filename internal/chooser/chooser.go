ddb5b1f5dbaaf6d5d485842c623691652ab9fe6f
Nate Woods <big.nate.w@gmail.com>
2024-12-04 23:18:27 -0700

    playing with a grouping helper

--- internal/chooser/chooser.go
+++ internal/chooser/chooser.go
@@ -64,6 +64,7 @@ type Chooser struct {
 	ownerPad int
 	ipPad    int
 	workers  int
+	sortMode uint8 // 0 = name, 1 = duration, moar?
 	quitting bool
 }
 
@@ -82,6 +83,10 @@ func (m *Chooser) Init() tea.Cmd {
 	n := len(m.table) + m.workers - 1
 	n /= m.workers
 
+	// FWIW, this code is dripping with new special sauce from the go team
+	// - ranging over an integer, pretty cool
+	// - loop capturing the range variable, pretty cool
+	// Just a lot of cool stuff here that could be bad in earlier versions of go!
 	batch := make([]tea.Cmd, m.workers)
 	for i := range m.workers {
 		batch[i] = func() tea.Msg {
@@ -102,14 +107,9 @@ func (m *Chooser) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case pleasePing:
 		if int(msg) >= len(m.table) || m.table[msg].status != "pending" {
 			m.workers--
-			if m.workers == 0 {
-				sort.Slice(m.table, func(i, j int) bool {
-					return m.table[i].duration < m.table[j].duration
-				})
-			}
 			return m, nil
 		}
-		m.table[msg].status = "..pinging"
+		m.table[msg].status = "........."
 		pinger := probing.New(m.table[msg].ip)
 		pinger.Count = 3
 		pinger.Interval = time.Millisecond * 50
@@ -130,16 +130,38 @@ func (m *Chooser) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.table[msg.index].duration = d
 			m.table[msg.index].status = fmt.Sprintf(`%.3fms`, d.Seconds()*1000)
 		}
-		msg.index++
 		return m, func() tea.Msg {
-			return pleasePing(msg.index)
+			return pleasePing(msg.index + 1)
 		}
 	case tea.Cmd:
 		return m, msg // allows please ping to send tea.Printf messages
 	case tea.KeyMsg:
-		if msg.String() == "q" {
+		switch msg.String() {
+		case "q":
 			m.quitting = true
 			return m, tea.Quit
+		case "s":
+			if m.workers != 0 {
+				return m, tea.Printf(`Can't sort while workers are running`)
+			}
+			m.sortMode = (m.sortMode + 1) % 2 // currently only 2 modes
+			switch m.sortMode {
+			case 0:
+				sort.Slice(m.table, func(i, j int) bool {
+					return m.table[i].owner < m.table[j].owner
+				})
+			case 1:
+				sort.Slice(m.table, func(i, j int) bool {
+					return m.table[i].duration < m.table[j].duration
+				})
+			}
+		case "g":
+			if m.workers != 0 {
+				return m, tea.Printf(`Can't group while workers are running`)
+			}
+			table := make([]row, len(m.table))
+			copy(table, m.table)
+			return newGrouper(table, m), nil
 		}
 	}
 	return m, nil
@@ -169,11 +191,12 @@ func (m *Chooser) View() string {
 		write(row.owner, row.ip, row.status)
 	}
 	line()
-	buff.WriteRune('\n')
 	if m.workers != 0 {
 		buff.WriteString(fmt.Sprintf("Working: %d\n", m.workers))
+	} else {
+		buff.WriteString("Press 's' to sort; 'g' to group\n")
 	}
-	return buff.String() + "Press 'q' to quit"
+	return buff.String() + fmt.Sprintf("Press 'q' to quit (%d hosts)", len(m.table))
 }
 
 func maxLength(s []string) (max int) {
@@ -183,4 +206,91 @@ func maxLength(s []string) (max int) {
 		}
 	}
 	return max
+}
+
+func newGrouper(table []row, revert tea.Model) *Grouper {
+
+	owners := make(map[string]groupedRow)
+	for _, row := range table {
+		data := owners[row.owner]
+		data.total += row.duration
+		data.count++
+		data.name = row.owner
+		owners[row.owner] = data
+	}
+
+	names := slices.Collect(maps.Keys(owners))
+	sort.Strings(names)
+
+	myTable := make([]groupedRow, 0, len(names))
+	for _, name := range names {
+		myTable = append(myTable, owners[name])
+	}
+
+	return &Grouper{table: myTable, revert: revert}
+}
+
+type groupedRow struct {
+	name  string
+	total time.Duration
+	count int
+}
+
+func (row groupedRow) avg() float64 {
+	return row.total.Seconds() * 1000 / float64(row.count)
+}
+
+type Grouper struct {
+	table    []groupedRow
+	sortMode uint8 // name, total, count, average, moar?
+	revert   tea.Model
+}
+
+func (m *Grouper) Init() tea.Cmd {
+	return nil
+}
+
+func (m *Grouper) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.Cmd:
+		return m, msg // allows please ping to send tea.Printf messages
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q":
+			return m, tea.Quit
+		case "g":
+			return m.revert, nil
+		case "s":
+			m.sortMode = (m.sortMode + 1) % 4 // currently only 2 modes
+			switch m.sortMode {
+			case 0:
+				sort.Slice(m.table, func(i, j int) bool {
+					return m.table[i].name < m.table[j].name
+				})
+			case 1:
+				sort.Slice(m.table, func(i, j int) bool {
+					return m.table[i].total < m.table[j].total
+				})
+			case 2:
+				sort.Slice(m.table, func(i, j int) bool {
+					return m.table[i].count < m.table[j].count
+				})
+			case 3:
+				sort.Slice(m.table, func(i, j int) bool {
+					return m.table[i].avg() < m.table[j].avg()
+				})
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m *Grouper) View() string {
+
+	var buff strings.Builder
+	for _, row := range m.table {
+		buff.WriteString(fmt.Sprintf("%s: %.3fms (ips: %d)\n", row.name, row.avg(), row.count))
+	}
+
+	return buff.String() + "\npress 'g' to revert; 's' to sort; 'q' to quit"
 }
